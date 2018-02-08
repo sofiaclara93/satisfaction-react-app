4d32e841dc344f6c930337bc2e68be09588ddca2
Sofia Cepeda <sofiaclara93@gmail.com>
2018-02-08 16:57:22 -0500

    add divs

--- csat-app/src/App.js
+++ csat-app/src/App.js
@@ -14,6 +14,15 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
     );
   }
 }
